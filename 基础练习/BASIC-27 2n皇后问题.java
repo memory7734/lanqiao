eaf1e0c31aa36855c8ccfd04374a2261ad8d98e4
memory7734 <memory7734@gmail.com>
2017-03-28 15:32:42 +0800

    完成2n皇后问题

--- 基础练习/BASIC-27 2n皇后问题.java
+++ 基础练习/BASIC-27 2n皇后问题.java
@@ -23,3 +23,64 @@
 样例输出
 0
  */
+import java.util.Scanner;
+public class Main {
+    public static int count=0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        WhiteQueen(board, new int[n], 0);
+        System.out.println(count);
+    }
+
+    public static void BlackQueen(int[][] board, int[] queen, int row) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+        for (int i = 0; i < queen.length; i++) {
+            if (board[row][i] == 1 && conflict(queen, row, i)) {
+                queen[row] = i;
+                BlackQueen(board, queen, row + 1);
+            }
+        }
+    }
+
+    public static void WhiteQueen(int[][] board, int[] queen, int row) {
+        if (row == board.length) {
+            int[][] boardCopy = new int[board.length][board.length];
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    boardCopy[i][j] = board[i][j];
+                }
+            }
+            for (int i = 0; i < board.length; i++) {
+                boardCopy[i][queen[i]] = 0;
+            }
+            BlackQueen(boardCopy, new int[queen.length], 0);
+            return;
+        }
+        for (int i = 0; i < queen.length; i++) {
+            if (board[row][i] == 1 && conflict(queen, row, i)) {
+                queen[row] = i;
+                WhiteQueen(board, queen, row + 1);
+            }
+        }
+    }
+
+    //判断第row行第j列对于queen数组是否可以摆放皇后
+    public static boolean conflict(int[] queen, int row, int j) {
+        for (int i = 0; i < row; i++) {
+            if (queen[i] == j || Math.abs(row - i) == Math.abs(queen[i] - j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
