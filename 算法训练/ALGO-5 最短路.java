a927e28b8f0d6732799ea43320f89d61af562576
memory7734 <memory7734@gmail.com>
2017-05-12 11:40:12 +0800

    add some solutions

--- 算法训练/ALGO-5 最短路.java
+++ 算法训练/ALGO-5 最短路.java
@@ -24,3 +24,48 @@
 
 对于100%的数据，1 <= n <= 20000，1 <= m <= 200000，-10000 <= l <= 10000，保证从任意顶点都能到达其他所有顶点。
  */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+        @Override
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = br.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+        Edge[] edges = new Edge[m];
+        int[] dist = new int[n];
+        int[] pre = new int[n];
+        for (int i = 1; i < n; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i < m; i++) {
+            edges[i] = new Edge();
+            strings = br.readLine().split(" ");
+            edges[i].src = Integer.parseInt(strings[0]) - 1;
+            edges[i].dest = Integer.parseInt(strings[1]) - 1;
+            edges[i].weight = Integer.parseInt(strings[2]);
+        }
+        Edge edge;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < edges.length; j++) {
+                edge = edges[j];
+                if (dist[edge.src] != Integer.MAX_VALUE && dist[edge.src] + edge.weight < dist[edge.dest]) {
+                    dist[edge.dest] = dist[edge.src] + edge.weight;
+                    pre[edge.dest] = edge.src;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            System.out.println(dist[i]);
+        }
+    }
+}
